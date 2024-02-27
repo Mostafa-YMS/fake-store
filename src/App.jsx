2bc851d8ce6d388f82e15b05058b7c70c5ecee43
Mostafa-YMS <mos_302@yahoo.com>
2024-02-27 23:03:40 +0200

    get products

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import { useCallback, useEffect, useState } from "react";
+import { getProductsService } from "./Services/getProducts";
 import "./App.scss";
 
 function App() {
+  const [products, setProducts] = useState();
+
+  const fetchProducts = useCallback(async () => {
+    const data = await getProductsService();
+    setProducts(data);
+  }, []);
+
+  useEffect(() => {
+    fetchProducts && fetchProducts();
+  }, [fetchProducts]);
+
   return <div className="main-container"></div>;
 }
 
