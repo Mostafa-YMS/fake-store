c4ab5e7ddf76320e0426b6d79723acc971b75aaf
Mostafa-YMS <mos_302@yahoo.com>
2024-02-29 12:15:24 +0200

    main grid

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,16 @@ function App() {
     fetchProducts && fetchProducts();
   }, [fetchProducts]);
 
-  return <div className="main-container"></div>;
+  return (
+    <div className="main-container">
+      <div style={{ width: "100%" }}>
+        <div className="main-grid">
+          <div className="best-sales"></div>
+          <div className="products-container"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
